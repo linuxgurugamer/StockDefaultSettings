1574fd73d3852023f3eef9b6587bac65d1ec4ee9
Jonathan Bayer <linuxgurugamer@gmail.com>
2020-10-15 10:49:26 -0400

    README changes

---
+++ StockDefaultSettings/Settings.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Default_Settings
+{
+    internal class Settings
+    {
+        internal static bool LocalSettings = false;
+        internal enum AntiAlias { _off = 0, _2x = 2, _4x = 4, _8x = 8 };
+        internal enum TextureQuality { FullRes = 0, HalfRes = 1, QuarterRes = 2, EighthRes = 3 };
+
+        internal bool HIGHLIGHT_FX;
+        internal bool INFLIGHT_HIGHLIGHT;
+        internal int TEMPERATURE_GAUGES_MODE;
+        internal bool CAMERA_DOUBLECLICK_MOUSELOOK;
+        internal bool ADVANCED_TWEAKABLES;
+        internal bool CONFIRM_MESSAGE_DELETION;
+        internal bool ADVANCED_MESSAGESAPP;
+        internal bool EXTENDED_BURNTIME;
+
+        internal AntiAlias ANTI_ALIASING;
+        internal TextureQuality TEXTURE_QUALITY;
+
+        internal Settings()
+        {
+            HIGHLIGHT_FX = GameSettings.HIGHLIGHT_FX;
+            INFLIGHT_HIGHLIGHT = GameSettings.INFLIGHT_HIGHLIGHT;
+            TEMPERATURE_GAUGES_MODE = GameSettings.TEMPERATURE_GAUGES_MODE;
+            CAMERA_DOUBLECLICK_MOUSELOOK = GameSettings.CAMERA_DOUBLECLICK_MOUSELOOK;
+            ADVANCED_TWEAKABLES = GameSettings.ADVANCED_TWEAKABLES;
+            CONFIRM_MESSAGE_DELETION = GameSettings.CONFIRM_MESSAGE_DELETION;
+            ADVANCED_MESSAGESAPP = GameSettings.ADVANCED_MESSAGESAPP;
+            EXTENDED_BURNTIME = GameSettings.EXTENDED_BURNTIME;
+
+            ANTI_ALIASING = (AntiAlias)GameSettings.ANTI_ALIASING;
+            TEXTURE_QUALITY = (TextureQuality)GameSettings.TEXTURE_QUALITY;
+        }
+        internal void Save(bool local = false)
+        {
+            GameSettings.HIGHLIGHT_FX = HIGHLIGHT_FX;
+            GameSettings.INFLIGHT_HIGHLIGHT = INFLIGHT_HIGHLIGHT;
+            GameSettings.TEMPERATURE_GAUGES_MODE = TEMPERATURE_GAUGES_MODE;
+            GameSettings.CAMERA_DOUBLECLICK_MOUSELOOK = CAMERA_DOUBLECLICK_MOUSELOOK;
+            GameSettings.ADVANCED_TWEAKABLES = ADVANCED_TWEAKABLES;
+            GameSettings.CONFIRM_MESSAGE_DELETION = CONFIRM_MESSAGE_DELETION;
+            GameSettings.ADVANCED_MESSAGESAPP = ADVANCED_MESSAGESAPP;
+            GameSettings.EXTENDED_BURNTIME = EXTENDED_BURNTIME;
+
+            GameSettings.ANTI_ALIASING = (int)ANTI_ALIASING;
+            GameSettings.TEXTURE_QUALITY = (int)TEXTURE_QUALITY;
+
+            if (local)
+            {
+                LocalSettings = true;
+            }
+        }
+    }
+}
+}
--- StockDefaultSettings/StockDefaultSettings.cs
+++ StockDefaultSettings/StockDefaultSettings.cs
@@ -7,67 +7,14 @@ using ClickThroughFix;
 
 namespace Stock_Default_Settings
 {
-    internal class Settings
-    {
-        internal static bool LocalSettings = false;
-        internal enum AntiAlias { _off = 0, _2x = 2, _4x = 4, _8x = 8 };
-        internal enum TextureQuality { FullRes = 0, HalfRes = 1, QuarterRes = 2, EighthRes = 3 };
-
-        internal bool HIGHLIGHT_FX;
-        internal bool INFLIGHT_HIGHLIGHT;
-        internal int TEMPERATURE_GAUGES_MODE;
-        internal bool CAMERA_DOUBLECLICK_MOUSELOOK;
-        internal bool ADVANCED_TWEAKABLES;
-        internal bool CONFIRM_MESSAGE_DELETION;
-        internal bool ADVANCED_MESSAGESAPP;
-        internal bool EXTENDED_BURNTIME;
-
-        internal AntiAlias ANTI_ALIASING;
-        internal TextureQuality TEXTURE_QUALITY;
-
-        internal Settings()
-        {
-            HIGHLIGHT_FX = GameSettings.HIGHLIGHT_FX;
-            INFLIGHT_HIGHLIGHT = GameSettings.INFLIGHT_HIGHLIGHT;
-            TEMPERATURE_GAUGES_MODE = GameSettings.TEMPERATURE_GAUGES_MODE;
-            CAMERA_DOUBLECLICK_MOUSELOOK = GameSettings.CAMERA_DOUBLECLICK_MOUSELOOK;
-            ADVANCED_TWEAKABLES = GameSettings.ADVANCED_TWEAKABLES;
-            CONFIRM_MESSAGE_DELETION = GameSettings.CONFIRM_MESSAGE_DELETION;
-            ADVANCED_MESSAGESAPP = GameSettings.ADVANCED_MESSAGESAPP;
-            EXTENDED_BURNTIME = GameSettings.EXTENDED_BURNTIME;
-
-            ANTI_ALIASING = (AntiAlias)GameSettings.ANTI_ALIASING;
-            TEXTURE_QUALITY = (TextureQuality)GameSettings.TEXTURE_QUALITY;
-        }
-        internal void Save(bool local = false)
-        {
-            GameSettings.HIGHLIGHT_FX = HIGHLIGHT_FX;
-            GameSettings.INFLIGHT_HIGHLIGHT = INFLIGHT_HIGHLIGHT;
-            GameSettings.TEMPERATURE_GAUGES_MODE = TEMPERATURE_GAUGES_MODE;
-            GameSettings.CAMERA_DOUBLECLICK_MOUSELOOK = CAMERA_DOUBLECLICK_MOUSELOOK;
-            GameSettings.ADVANCED_TWEAKABLES = ADVANCED_TWEAKABLES;
-            GameSettings.CONFIRM_MESSAGE_DELETION = CONFIRM_MESSAGE_DELETION;
-            GameSettings.ADVANCED_MESSAGESAPP = ADVANCED_MESSAGESAPP;
-            GameSettings.EXTENDED_BURNTIME = EXTENDED_BURNTIME;
-
-            GameSettings.ANTI_ALIASING =(int) ANTI_ALIASING;
-            GameSettings.TEXTURE_QUALITY = (int)TEXTURE_QUALITY;
-
-            if (local)
-            {
-                LocalSettings = true;
-            }
-        }
-    }
 
     [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
     public partial class StockDefaultSettings : MonoBehaviour
     {
         const string ALT = "Use Alt.Skin";
-        const float REVERTWIDTH = 450;
-        const float REVERTHEIGHT = 100;
-        Rect _revertRect = new Rect((Screen.width - REVERTWIDTH) * .75f, (Screen.height - REVERTHEIGHT) / 2, REVERTWIDTH, REVERTHEIGHT);
-        Rect _revertRect2 = new Rect((Screen.width - REVERTWIDTH) * .25f, (Screen.height - REVERTHEIGHT) / 2, REVERTWIDTH, REVERTHEIGHT);
+        const float SDSWIDTH = 450;
+        const float SDSHEIGHT = 100;
+        Rect sdsRect = new Rect((Screen.width - SDSWIDTH) /2, Screen.height - (Screen.height - SDSHEIGHT) / 2, SDSWIDTH, SDSHEIGHT);
 
         bool useAltSkin = true;
 
@@ -109,7 +56,7 @@ namespace Stock_Default_Settings
             {
                 if (!useAltSkin)
                     GUI.skin = HighLogic.Skin;
-                _revertRect = GUILayout.Window(this.GetInstanceID() + 1, _revertRect, new GUI.WindowFunction(Window), "Stock Default Settings", new GUILayoutOption[0]);
+                sdsRect = GUILayout.Window(this.GetInstanceID() + 1, sdsRect, new GUI.WindowFunction(Window), "Stock Default Settings", new GUILayoutOption[0]);
             }
         }
 
@@ -184,6 +131,11 @@ namespace Stock_Default_Settings
             {
                 settings.Save();
                 visible = false;
+            }
+            if (GUILayout.Button("Reset to default", GUILayout.MinWidth(60)))
+            {
+                Settings.LocalSettings = false;
+                ScenarioData.Instance.OnLoad(null);
             }
             GUILayout.FlexibleSpace();
             if (GUILayout.Button("Cancel", GUILayout.Width(60)))
